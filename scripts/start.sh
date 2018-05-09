76fbe3f253ee582688b9c785d09246a7ada3ee16
Vinicius Canaa Medeiros Ruela <vcanaa@gmail.com>
2018-05-08 17:38:49 -0700

    Improving logging in driver (#71)
    * Improving logging experience
    
    * Fail fast in driver
    
    * Use deployer status to tell if it succeeded or failed

--- scripts/start.sh
+++ scripts/start.sh
@@ -14,9 +14,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-set -x
-set -e
-set -o pipefail
+set -eo pipefail
 
 for i in "$@"
 do
@@ -163,4 +161,5 @@ spec:
       - name: config-volume
         configMap:
           name: "${name}-deployer-config"
+  backoffLimit: 0
 EOF
