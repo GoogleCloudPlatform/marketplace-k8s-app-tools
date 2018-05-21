c91d74dd7c77b42536dd56bfe77612744b3af771
TK Kiatkungwanglai <trironk@gmail.com>
2018-05-21 15:39:09 -0700

    Added utilities for processing configuration data. (#101)
    * Adding scripts/extract_deployer_config_schema.sh.
    
    * Added utilities for parsing schema and values.
    
    * Addressing PR feedback.

---
+++ scripts/extract_deployer_config_schema.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+#
+# Copyright 2018 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -eo pipefail
+
+for i in "$@"
+do
+case $i in
+  --deployer=*)
+    deployer="${i#*=}"
+    shift
+    ;;
+  *)
+    >&2 echo "Unrecognized flag: $i"
+    exit 1
+    ;;
+esac
+done
+
+[[ -z "$deployer" ]] && >&2 echo "--deployer required" && exit 1
+
+docker run --entrypoint="/bin/bash" --rm "$deployer" -c 'cat /data/schema.yaml'
