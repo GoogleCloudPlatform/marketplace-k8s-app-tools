c6526e31e4e1aeee58c77fb97a20a469bd3ad0c4
Kamil <khajduczenia@google.com>
2018-06-04 14:07:22 +0200

    Ensure k8s apps labels (#111)
    * Automatically add labels following the K8s Apps convention - app.kubernetes.io/name

--- marketplace/deployer_helm_base/create_manifests.sh
+++ marketplace/deployer_helm_base/create_manifests.sh
@@ -91,6 +91,10 @@ for chart in "$data_dir/extracted"/*; do
      --manifest "$manifest_dir/$chart_manifest_file" \
      --tests-manifest "$test_data_dir/extracted/helm-tests-$chart_manifest_file"
   fi
+
+  ensure_k8s_apps_labels.py \
+    --manifest "$manifest_dir/$chart_manifest_file" \
+    --appname "$NAME"
 done
 
 /bin/setownership.py \
