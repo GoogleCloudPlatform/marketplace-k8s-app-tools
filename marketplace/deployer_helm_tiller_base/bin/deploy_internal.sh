1481cdc1bfa9e7da1ecf9351603174f66bfb8051
TK Kiatkungwanglai <trironk@gmail.com>
2018-10-17 13:04:03 -0700

    Add new Helm deployer leveraging local-tiller for stronger integration. (#221)
    * Create application via helm/tiller.
    
    * Initialization commit.
    
    This commit initializes the marketplace/deployer_helm_tiller_base/ directory in the following manner:
    
    - Copy deploy.sh and deploy_with_tests.sh to marketplace/deployer_helm_tiller_base/bin/.
    - Run `helm fetch stable/chartmuseum --untar --destination marketplace/deployer_helm_tiller_base/test/`.
    
    This is done to minimize the diff for code review (see next commit).
    
    * Incorporate wrapper helm chart.
    
    * Extract hook yamls into chart.
    
    * Incorporate APPLICATION_UID and address PR feedback.
    
    * Update maintainer email.
    
    * Move marketplace/deployer_helm_tiller_base/test to testing/.
    
    * Rework marketplace chart and references.
    
    * Parameterize the chart build process.
    
    * Address PR feedback.
    
    * Add ONBUILD directives to deployer_helm_tiller_base.
    
    * Update marketplace-hooks maintainer.
    
    * Rename marketplace-hooks to marketplace-integration.
    
    Also retrofit print_config.py invocations.
    
    * Address PR feedback.
    
    * Add secondary test case to exercise flow for partners not using requirements.yaml.
    
    * Add back helm build dependency.
    
    * Retrofit with functionality from https://github.com/GoogleCloudPlatform/marketplace-k8s-app-tools/pull/241.
    
    * Adjust listing template.
    
    * Remove marketplace-integration chart build process.
    
    * Separate standard and onbuild helm tiller test cases and rework testing.Makefile.
    
    * Add cloudbuild.yaml build for tiller images.
    
    * Address PR feedback.

---
+++ marketplace/deployer_helm_tiller_base/bin/deploy_internal.sh
@@ -0,0 +1,102 @@
+#!/bin/bash
+#
+# Copyright 2018 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -eox pipefail
+
+app_uid=$(kubectl get "applications/$NAME" \
+  --namespace="$NAMESPACE" \
+  --output=jsonpath='{.metadata.uid}')
+app_api_version=$(kubectl get "applications/$NAME" \
+  --namespace="$NAMESPACE" \
+  --output=jsonpath='{.apiVersion}')
+
+for chart in /data/chart/*; do
+  # Expand and apply the template for the Application resource.
+  # Note: This must be done out-of-band because the Application resource
+  # is created before the deployer is invoked, but helm does not handle
+  # pre-existing resources.
+  helm template \
+      --name="$NAME" \
+      --namespace="$NAMESPACE" \
+      --values=<(print_config.py --output=yaml) \
+      --set "$(print_config.py --xtype APPLICATION_UID --key true)=" \
+      "$chart" \
+    | yaml2json \
+    | jq 'select( .kind == "Application" )' \
+    | kubectl apply -f -
+
+  # Install the chart.
+  # Note: This does not assume that tiller is installed in the cluster;
+  # it runs a local tiller process to perform template expansion and
+  # hook processing.
+  # Note: The local tiller process is configured with --storage=secret,
+  # which is recommended but not default behavior.
+  helm tiller run "$NAMESPACE" -- \
+    helm install \
+        --name="$NAME" \
+        --namespace="$NAMESPACE" \
+        --values=<(print_config.py --output=yaml) \
+        "$chart"
+
+  # Establish an ownerReference back to the Application resource, so that
+  # the helm release will be cleaned up when the Application is deleted.
+  # Note: This is fragile, as any time that tiller updates the release
+  # it will remove the ownerReference. Also, any future Secret resources
+  # created by tiller will not have this ownerReference established.
+  patch="$(echo '{}' \
+    | jq '{
+            "metadata": {
+              "ownerReferences": [
+                {
+                  "apiVersion": $app_api_version,
+                  "kind": "Application",
+                  "name": $name,
+                  "uid": $app_uid,
+                  "blockOwnerDeletion": true
+                }
+              ],
+              "labels": {
+                "app.kubernetes.io/name": $name,
+                "app.kubernetes.io/namespace": $namespace
+              }
+            }
+          }' \
+          --arg name "$NAME" \
+          --arg namespace "$NAMESPACE" \
+          --arg app_uid "$app_uid" \
+          --arg app_api_version "$app_api_version")"
+
+  kubectl get secrets \
+      --namespace="$NAMESPACE" \
+      --selector="OWNER=TILLER,NAME=$NAME" \
+      --output=json \
+    | jq '.items[]
+            | {
+                apiVersion: .apiVersion,
+                kind: .kind,
+                metadata: {
+                  namespace: .metadata.namespace,
+                  name: .metadata.name
+                }
+              }
+         ' \
+      --arg name "$NAME" \
+      --arg namespace "$NAMESPACE" \
+    | kubectl patch \
+        --namespace="$NAMESPACE" \
+        --patch="$patch" \
+        --filename -
+done
---
+++ testing/marketplace/deployer_helm_tiller_base/onbuild/helm-dependency-build/Dockerfile
@@ -0,0 +1,2 @@
+ARG MARKETPLACE_TOOLS_TAG
+FROM gcr.io/cloud-marketplace-tools/k8s/deployer_helm_tiller/onbuild:$MARKETPLACE_TOOLS_TAG
