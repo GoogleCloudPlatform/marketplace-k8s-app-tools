a6cb8354e789e160c3158cd365f2eedbdac15eb0
Vini Ruela <ruela@google.com>
2019-06-19 15:47:05 -0700

    Improve logging of verify script

---
+++ scripts/filter_deployer_logs.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python2
+
+import sys
+import re
+
+# Extract useful information from deployer logs, based on common errors found in the past.
+
+content = sys.stdin.read()
+
+# Python exceptions
+m = re.search("Traceback.*\n(.*(  )+.*\n)*.*\n", content)
+if m:
+  sys.stdout.write(m.group(0))
+
+# Tar errors.
+m = re.search("tar: .*: Cannot open: No such file or directory", content)
+if m:
+  sys.stdout.write(m.group(0))
