39787a41c0bb5671af9dc218c0e38ea675a50a7a
Erika Shiroma <eshiroma@google.com>
2019-07-16 11:11:49 -0700

    Run 'make format/py'

--- marketplace/deployer_util/expand_config_test.py
+++ marketplace/deployer_util/expand_config_test.py
@@ -44,8 +44,8 @@ class ExpandConfigTest(unittest.TestCase):
           p1:
             type: string
         """)
-    self.assertRaises(expand_config.InvalidProperty,
-                      lambda: expand_config.expand({'p1': 3}, schema))
+    self.assertRaises(expand_config.InvalidProperty, lambda: expand_config.
+                      expand({'p1': 3}, schema))
 
   def test_generate_properties_for_v1_image_split_by_colon(self):
     schema = config_helper.Schema.load_yaml("""
